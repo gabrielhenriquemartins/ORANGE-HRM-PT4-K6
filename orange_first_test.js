29899d46baef37b71cfe9d9705e99eddf50a6dc8
Gabriel Henrique Martins <biellhenri@hotmail.com>
2024-11-28 02:11:50 -0300

    ADD: Authentication Requests
    Signed-off-by: Gabriel Henrique Martins <biellhenri@hotmail.com>

--- orange_first_test.js
+++ orange_first_test.js
@@ -1,26 +1,64 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
-import { check } from 'k6';
+import { sleep, check } from 'k6';
+
+let token = null;
+let cookies = {};
 
 export const options = {
+    maxRedirects: 0,
     stages: [
         {
             duration: '5s',
             target: 1
         }
     ]
-}
+};
 
 export default function () {
     const res = http.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-    sleep(5)
-    console.log(res.body)
+    sleep(5);
+
     const regex = /:token="&quot;([^"]+)/;
-    const match = String(res.body).match(regex);
-    if (match) {
-        const token = match[1].replace(/&quot;/g, ''); // Clean up any remaining &quot; if necessary
-        console.log(`Extracted Token: ${token}`);
-    } else {
-    console.log('Token not found in the response.');
+
+    const result = check(res, {
+        'Auth Login: Status Code is 200': (r) => r.status === 200,
+        'Valid Token Found': (r) => {
+            const match = String(r.body).match(regex);
+            if (match) {
+                token = match[1].replace(/&quot;/g, '');
+                console.log(`Token: ${token}`);
+                return true;
+            } else {
+                console.log('Token not found.');
+                return false;
+            }
+        },
+    });
+
+    if (!result) {
+        console.error('Some tests failed!');
+        return;
     }
+
+    const payload = {
+        _token: token,
+        username: 'Admin',
+        password: 'admin123',
+    };
+    const params = {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        follow: false
+    };
+
+    const loginRes = http.post('https://opensource-demo.orangehrmlive.com/web/index.php/auth/validate', payload, params);
+    
+    check(loginRes, {
+        'Auth Validation: Status Code is 302': (r) => r.status === 302,
+        'Valid Cookie Found': (r) => r.cookies.session !== null,
+    });
+
+    cookies = loginRes.cookies;
+    console.log('Login successful! Cookies:', cookies);
 }
