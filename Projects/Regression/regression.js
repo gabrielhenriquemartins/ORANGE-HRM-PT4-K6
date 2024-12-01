32f9656597cc95e35babb340c373d7d23a685710
Gabriel Henrique Martins <biellhenri@hotmail.com>
2024-12-01 03:35:23 -0300

    ADD: Email configuration
    Signed-off-by: Gabriel Henrique Martins <biellhenri@hotmail.com>

--- Projects/Regression/regression.js
+++ Projects/Regression/regression.js
@@ -1,15 +1,21 @@
 import { smokeTestStages, loadTestStages } from './regression_stages.js';
 import { getLoginPage, login } from "../../Resources/login/login.js";
-import { createJobTitle, deleteJobTitle } from '../../Resources/admin/admin.js';
+import { createJobTitle, deleteJobTitle, emailConfiguration } from '../../Resources/admin/admin.js';
 import { randomString } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 export const options = (__ENV.EXECUTION_TYPE === 'load') ? loadTestStages : smokeTestStages; // If true, run load; else run smoke. Default is smoke.
 
 export default function () {
+    //************* Login *************
     const token = getLoginPage();
     const cookies = login(token, 'Admin', 'admin123', 302);
+    //*********************************
 
+    //************* Admin *************
     const jobID = createJobTitle(cookies, 'New Job - ' + randomString(6), 'My description', 'My Note', 200);
-
     deleteJobTitle(cookies, jobID, 200);
+    emailConfiguration(cookies, 200);
+    //*********************************
+
+    //************* Claim *************
 }
--- Resources/admin/admin.js
+++ Resources/admin/admin.js
@@ -62,3 +62,38 @@ export function deleteJobTitle(cookies, jobID, expectedStatusCode) {
         console.log(`Job title deleted successfully! ID: ${jobID}`);
     }
 }
+
+export function emailConfiguration(cookies, expectedStatusCode) {
+    const payload = JSON.stringify({
+        "mailType": "sendmail",
+        "sentAs": "send@mail.com",
+        "smtpHost": null,
+        "smtpPort": null,
+        "smtpUsername": "",
+        "smtpPassword": null,
+        "smtpAuthType": "none",
+        "smtpSecurityType": "none",
+        "testEmailAddress": "destination@mail.com"
+    });
+
+    const params = {
+        headers: {
+            "Content-Type": "application/json",
+            Cookies: cookies,
+        }
+    };
+
+    const res = http.put('https://opensource-demo.orangehrmlive.com/web/index.php/api/v2/admin/email-configuration', payload, params);
+
+    sleep(3)
+
+    const valid = check(res, {
+        [`Email Configuration: Status Code is ${expectedStatusCode}`]: (r) => r.status === expectedStatusCode,
+    });
+
+    if (!valid) {
+        throw new Error('Email configuration failed');
+    }else{
+        console.log(`Email configured successfully!`);
+    }
+}
